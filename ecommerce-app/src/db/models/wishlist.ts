e577073f9ce5682d872116e4f7785325ececbdd3
fauzandwip <fauzan27dp@gmail.com>
2023-12-09 15:46:28 +0700

    feat: add configuration to show all product wishlist user

--- ecommerce-app/src/db/models/wishlist.ts
+++ ecommerce-app/src/db/models/wishlist.ts
@@ -1,33 +1,55 @@
 import { ObjectId } from 'mongodb';
 import { getDB } from '../config';
+import { Product } from './products';
 
-interface NewWishlist {
+export interface NewWishlist {
 	userId: string;
 	productId: string;
 }
 
-interface Wishlist extends NewWishlist {
+export interface WishlistModel extends NewWishlist {
 	_id: ObjectId;
 	createdAt: Date;
 	updatedAt: Date;
+	product: Product;
 }
 
 export const addWishlist = async (data: NewWishlist) => {
 	const currentDate = new Date();
 	const db = await getDB();
 	const response = await db.collection('Wishlist').insertOne({
-		...data,
+		userId: new ObjectId(data.userId),
+		productId: new ObjectId(data.productId),
 		createdAt: currentDate,
 		updatedAt: currentDate,
 	});
 	return response;
 };
 
-export const getWishlist = async (userId: string): Promise<Wishlist[]> => {
+export const getWishlist = async (userId: string): Promise<WishlistModel[]> => {
 	const db = await getDB();
 	const response = (await db
 		.collection('Wishlist')
-		.find({ userId: new ObjectId(userId) })
-		.toArray()) as Wishlist[];
+		.aggregate([
+			{ $match: { userId: new ObjectId(userId) } },
+			{
+				$lookup: {
+					from: 'Products',
+					foreignField: '_id',
+					localField: 'productId',
+					as: 'product',
+				},
+			},
+			{
+				$unwind: {
+					path: '$product',
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+		])
+		// .find({ userId: new ObjectId(userId) })
+		.toArray()) as WishlistModel[];
+	// console.log(response, 'wishlist db');
+
 	return response;
 };

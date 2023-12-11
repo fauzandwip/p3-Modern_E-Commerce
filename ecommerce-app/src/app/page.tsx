70e66d8f090eff22c110fd8978d9aabc90e87287
fauzandwip <fauzan27dp@gmail.com>
2023-12-11 17:44:50 +0700

    fix: fix code while deploy

--- ecommerce-app/src/app/page.tsx
+++ ecommerce-app/src/app/page.tsx
@@ -1,5 +1,23 @@
-import Home from '@/pages/Home';
+import Banner from '@/components/Banner';
+import CardList from '@/components/CardList';
+import SeeAllButton from '@/components/SeeAllButton';
+import ShopInformation from '@/components/ShopInformation';
 
 export default function HomePage() {
-	return <Home />;
+	return (
+		<div className="flex flex-col w-full bg-zinc-950 pt-24">
+			<div className="flex w-full px-16 mt-14">
+				<div className="flex-1 pe-10 items-center">
+					<ShopInformation />
+				</div>
+				<div className="flex-1">
+					<Banner />
+				</div>
+			</div>
+			<CardList />
+			<div className="w-full my-6 text-center">
+				<SeeAllButton />
+			</div>
+		</div>
+	);
 }
